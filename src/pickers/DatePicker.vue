0d41c4d1a879a05723ff09e60b3e98d7798d85e4
Alan Smith <smithalan2@gmail.com>
2026-03-28 00:27:24 +0000

    Always use native date/time pickers

--- src/pickers/DatePicker.vue
+++ src/pickers/DatePicker.vue
@@ -1,22 +1,14 @@
 <script setup lang="ts">
-import isMobileDevice from "@/utils/isMobile";
-import { VueDatePicker } from "@vuepic/vue-datepicker";
+import { watch } from "vue";
 
 const date = defineModel<string>();
+
+watch(date, (val) => {
+  console.log(val);
+});
 </script>
 <template>
   <div>
-    <VueDatePicker
-      v-if="!isMobileDevice"
-      class="w-full"
-      v-model="date"
-      :time-config="{ enableTimePicker: false }"
-      :teleport="true"
-      model-type="yyyy-MM-dd"
-      format="do MMM yyyy"
-      auto-apply
-      dark
-    />
-    <input v-if="isMobileDevice" class="et-input" type="date" v-model="date" />
+    <input class="et-input" type="date" v-model="date" />
   </div>
 </template>
--- src/pickers/TimePicker.vue
+++ src/pickers/TimePicker.vue
@@ -1,26 +1,8 @@
 <script setup lang="ts">
-import isMobileDevice from "@/utils/isMobile";
-import { VueDatePicker } from "@vuepic/vue-datepicker";
-
 const time = defineModel<string>();
 </script>
 <template>
   <div class="w-full">
-    <VueDatePicker
-      v-if="!isMobileDevice"
-      class="w-full"
-      v-model="time"
-      model-type="HH:mm"
-      :teleport="true"
-      time-picker
-      auto-apply
-      dark
-    />
-    <input
-      v-if="isMobileDevice"
-      class="et-input"
-      type="time"
-      v-model="time"
-    />
+    <input class="et-input" type="time" v-model="time" />
   </div>
 </template>
